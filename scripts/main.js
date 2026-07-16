96c2f27e6a555558c598a6810e8d74b506be03de
Gabriel Rousseau <2491012@cegepmontpetit.ca>
2026-07-16 15:54:01 -0400

    pages changes

--- scripts/main.js
+++ scripts/main.js
@@ -46,38 +46,10 @@ async function boot() {
             if (currentPath === link.pathname) {
                 link.classList.add("activeLink");
 
-                // check if is in a dropdown
-                const dropdownContainer = link.closest('.headerDropdown');
-                if (dropdownContainer) {
-                    const parentWrapper = dropdownContainer.closest('.headerLink');
-                    if (parentWrapper) {
-                        const parentButton = parentWrapper.querySelector('a');
-                        if (parentButton) {
-                            parentButton.classList.add("activeLink");
-                        }
-                    }
-                }
 
                 break;
             }
         }
-
-
-
-
-        // dropdown behavior
-
-        const moreDropdownBtn = document.getElementById("dropdownBtn-more");
-        const moreDropdown = document.getElementById("dropdown-more");
-        const dropdownWrapper = moreDropdownBtn.parentElement;
-
-        dropdownWrapper.addEventListener("mouseenter", () => {
-            moreDropdown.classList.add("active");
-        });
-
-        dropdownWrapper.addEventListener("mouseleave", () => {
-            moreDropdown.classList.remove("active");
-        });
 
     }
 
